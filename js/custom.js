b19a4414f620668bfabe051d14f05af3eaf0c584
mehmetkarakus <mehmetkrks438@gmail.com>
2024-02-13 12:26:57 +0300

    update

--- js/custom.js
+++ js/custom.js
@@ -1,8 +1,13 @@
-/* 
+window.addEventListener("scroll", function() {
+    var beyazLogo = document.querySelector(".logo__white");
+    var siyahLogo = document.querySelector(".logo__dark");
+    var threshold = 100; // Scroll eşiği, logonun ne zaman görünür hale geleceğini belirler
 
-1. Add your custom JavaScript code below
-2. Place the this code in your template:
-
-  
-
-*/
+    if (window.pageYOffset > threshold) {
+        beyazLogo.style.display = "none";
+        siyahLogo.style.display = "block";
+    } else {
+        beyazLogo.style.display = "block";
+        siyahLogo.style.display = "none";
+    }
+});
